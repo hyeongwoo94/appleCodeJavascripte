48ea98621d4b6045f28e9651b741cdc7c9422f50
hyeongwoo94 <guddn0625@gmail.com>
2023-05-15 11:59:32 +0900

    array,object

--- tab.js
+++ tab.js
@@ -1,18 +1,25 @@
-for(let i= 0; i < document.getElementsByClassName('tab-button').length; i++){
-    document
-    .querySelectorAll(".tab-button")[i]
-    .addEventListener("click", function () {
-    document.querySelectorAll(".tab-content")[0].classList.remove("show");
-    document.querySelectorAll(".tab-content")[1].classList.remove("show");
-    document.querySelectorAll(".tab-content")[2].classList.remove("show");
-    document.querySelectorAll(".tab-content")[i].classList.add("show");
-    document.querySelectorAll(".tab-button")[0].classList.remove("orange");
-    document.querySelectorAll(".tab-button")[1].classList.remove("orange");
-    document.querySelectorAll(".tab-button")[2].classList.remove("orange");
-    document.querySelectorAll(".tab-button")[i].classList.add("orange");
+document.querySelector(".list").addEventListener("click", function (e) {
+  console.log(e.target.dataset.id);
+  탭열기(e.target.dataset.id);
+});
+// for (let i = 0; i < document.getElementsByClassName("tab-button").length; i++) {
+//   탭열기(i);
+// }
+
+function 탭열기(구멍) {
+  document
+    .querySelectorAll(".tab-button")
+    [구멍].addEventListener("click", function () {
+      document.querySelectorAll(".tab-content")[0].classList.remove("show");
+      document.querySelectorAll(".tab-content")[1].classList.remove("show");
+      document.querySelectorAll(".tab-content")[2].classList.remove("show");
+      document.querySelectorAll(".tab-content")[구멍].classList.add("show");
+      document.querySelectorAll(".tab-button")[0].classList.remove("orange");
+      document.querySelectorAll(".tab-button")[1].classList.remove("orange");
+      document.querySelectorAll(".tab-button")[2].classList.remove("orange");
+      document.querySelectorAll(".tab-button")[구멍].classList.add("orange");
     });
 }
-
 // document
 // .querySelectorAll(".tab-button")[0]
 // .addEventListener("click", function () {
@@ -51,5 +58,3 @@ for(let i= 0; i < document.getElementsByClassName('tab-button').length; i++){
 //   document.querySelectorAll(".tab-button")[2].classList.remove("orange");
 //   document.querySelectorAll(".tab-button")[2].classList.add("orange");
 // });
-
-
